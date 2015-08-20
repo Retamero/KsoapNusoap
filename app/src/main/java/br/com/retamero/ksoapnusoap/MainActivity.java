a798a10fbafb00dc15a7864b7fb4732493b7abc3
Aguinaldo Retamero <retamer.slackware@gmail.com>
2015-08-20 17:44:44 -0300

    Tentativa de implementar metodo UPDATE

--- app/src/main/java/br/com/retamero/ksoapnusoap/MainActivity.java
+++ app/src/main/java/br/com/retamero/ksoapnusoap/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.retamero.ksoapnusoap;
 
 
+import org.apache.http.entity.StringEntity;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.ksoap2.SoapEnvelope;
@@ -18,20 +19,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.app.Activity;
 import android.widget.Toast;
+
+import java.net.HttpURLConnection;
+
 import static br.com.retamero.ksoapnusoap.R.id.button;
+import static br.com.retamero.ksoapnusoap.R.id.button2;
 
 public class MainActivity extends Activity {
 	/* Configurações que devem estar no lado WEBSERVICE do PHP/APACHE/WINDOWS ou LINUX
 	 * mudar onde esta retamero.com.br pelo IP ou dominio da maquina que esta rodando o webservice.
 	 */
 	private static final String SOAP_ACTION = "http://retamero.com.br/webservice/server.php/exemplo";
+	private static final String SOAP_ACTION_UPDATE = "http://retamero.com.br/webservice/server.php/update";
 	private static final String METHOD_NAME = "exemplo";
+	private static final String METHOD_UPDATE = "update";
 	private static final String NAMESPACE = "http://retamero.com.br/webservice";
 	private static final String URL = "http://retamero.com.br/webservice/server.php?wsdl";
 	private TextView tv, tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8, tv9, tv10, tv11, tv12, tv13, tv14, tv15, tv16;
-	private EditText etTexto;
+	private EditText etTexto, ed1, ed2, ed3, ed4, ed5, ed6, ed7, ed8, ed9, ed10, ed11, ed12, ed13, ed14, ed15;
 	private String response;
-	private String msg;
+	private String msg, envia;
 
 	public void limpaCampos(){
 		tv1.setText("");
@@ -82,9 +89,27 @@ public class MainActivity extends Activity {
 		tv14 = (TextView) findViewById(R.id.textView32); //padi
 		tv15 = (TextView) findViewById(R.id.textView33); //ques
 		tv16 = (TextView) findViewById(R.id.textView34); //qudi
+
 		etTexto = (EditText) findViewById(R.id.editText2);//envia
-		Button btn = (Button) findViewById(button);
-		btn.setOnClickListener(new View.OnClickListener() {
+		ed1 = (EditText) findViewById(R.id.editText3); //peso
+		ed2 = (EditText) findViewById(R.id.editText4); //altura
+		ed3 = (EditText) findViewById(R.id.editText5); //pema
+		ed4 = (EditText) findViewById(R.id.editText6); //peme
+		ed5 = (EditText) findViewById(R.id.editText7); //quadril
+		ed6 = (EditText) findViewById(R.id.editText8); //bies
+		ed7 = (EditText) findViewById(R.id.editText9); //bidi
+		ed8 = (EditText) findViewById(R.id.editText10); //tres
+		ed9 = (EditText) findViewById(R.id.editText11);  //trdi
+		ed10 = (EditText) findViewById(R.id.editText12); //coes
+		ed11 = (EditText) findViewById(R.id.editText13); //codi
+		ed12 = (EditText) findViewById(R.id.editText14); //paes
+		ed13 = (EditText) findViewById(R.id.editText15); //padi
+		ed14 = (EditText) findViewById(R.id.editText16); //ques
+		ed15 = (EditText) findViewById(R.id.editText17); //qudi
+
+		Button btnEnviar = (Button) findViewById(button);
+		Button btnEditar = (Button) findViewById(button2);
+		btnEnviar.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				limpaCampos();
 				String texto = etTexto.getText().toString();
@@ -95,6 +120,21 @@ public class MainActivity extends Activity {
 				}else {
 					myAsyncTask myRequest = new myAsyncTask();
 					myRequest.execute();
+				}
+			}
+		});
+
+		btnEditar.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				limpaCampos();
+				String texto = etTexto.getText().toString();
+				if(texto.equals("") || texto == null){ //tratamento de erro iserção vazia
+					msg = "ERR0: Um numero precisa ser especificado..";
+					tv.setText(msg);
+					notifica(msg);
+				}else {
+					editarAsynTask myRequestEditar = new editarAsynTask();
+					myRequestEditar.execute();
 				}
 			}
 		});
@@ -173,11 +213,78 @@ public class MainActivity extends Activity {
 
 			if (result != null) {
 				response = result.getProperty(0).toString();
-				Log.i("TAG", response);
+				Log.i("SELECT", response);
 
 			} else {
-				Log.i("TAG", "====> NULO <========");
+				Log.i("SELECT", "====> NULO <========");
 				response = "{\"id\":\"null\"}";
+			}
+			return null;
+		}
+	}
+
+	private class editarAsynTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			try {
+				JSONObject objReceive = new JSONObject(response);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+		}
+
+		@Override
+		protected Void doInBackground(Void... arg0) {
+			try {
+				JSONObject objSend = new JSONObject();
+				objSend.accumulate("peso", ed1.getText().toString());
+				objSend.accumulate("altura", ed2.getText().toString());
+				objSend.accumulate("peitoral_maior", ed3.getText().toString());
+				objSend.accumulate("peitoral_menor", ed4.getText().toString());
+				objSend.accumulate("quadril", ed5.getText().toString());
+				objSend.accumulate("biceps_esquerdo", ed6.getText().toString());
+				objSend.accumulate("biceps_direito", ed7.getText().toString());
+				objSend.accumulate("triceps_esquerdo", ed8.getText().toString());
+				objSend.accumulate("triceps_direito", ed9.getText().toString());
+				objSend.accumulate("coxas_esquerda", ed10.getText().toString());
+				objSend.accumulate("coxas_direita", ed11.getText().toString());
+				objSend.accumulate("panturrilha_esquerda", ed12.getText().toString());
+				objSend.accumulate("panturrilha_direita", ed13.getText().toString());
+				objSend.accumulate("quadril_esquerdo", ed14.getText().toString());
+				objSend.accumulate("quadril_direito", ed15.getText().toString());
+				Log.i("DEBUG", "ENVIA " + objSend.toString());
+
+				SoapObject request = new SoapObject(NAMESPACE, METHOD_UPDATE);
+				request.addProperty("id", etTexto.getText().toString());
+				request.addProperty("id", objSend.toString());
+				SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+					SoapEnvelope.VER11);
+				envelope.bodyOut = request;
+    			envelope.encodingStyle = SoapSerializationEnvelope.ENC2003;
+    			envelope.dotNet = true;
+    			envelope.setOutputSoapObject(request);
+			HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+			androidHttpTransport.call(SOAP_ACTION_UPDATE, envelope);
+
+				SoapObject result;
+				result = (SoapObject) envelope.bodyIn;
+				if (result != null) {
+					response = result.getProperty(0).toString();
+					Log.i("UPDATE", response);
+
+				} else {
+					Log.i("UPDATE", "====> NULO <========");
+					response = "{\"id\":\"null\"}";
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 			return null;
 		}
